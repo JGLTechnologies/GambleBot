28429827ee4660d3a3b8c3a2349b6575e5922de6
Nebulizer1213 <fixingg@gmail.com>
2022-01-11 21:41:05 -0500

    update

--- cogs/shop.py
+++ cogs/shop.py
@@ -46,7 +46,8 @@ class Shop(commands.Cog):
             async with db.execute("SELECT member,guild,last_paid FROM security") as cursor:
                 async for entry in cursor:
                     member_id, guild_id, last_paid = entry
-                    if last_paid >= time.time() - 24 * 3600:
+                    if last_paid <= time.time() - (24 * 3600):
+                        print(last_paid, time.time() - (24 * 3600))
                         bal = await get_balance(guild_id, member_id)
                         guild = self.bot.get_guild(guild_id)
                         channel = guild.get_channel(await get_channel(guild_id, "bills"))
@@ -57,8 +58,12 @@ class Shop(commands.Cog):
                             await remove_security(guild_id, member_id)
                         else:
                             await set_balance(guild_id, member_id, bal - 5000)
-                            await channel.send(
-                                f"{guild.get_member(member_id).mention}, you have been charged $5000 for your daily security bill.")
+                            if channel is not None:
+                                await channel.send(
+                                    f"{guild.get_member(member_id).mention}, you have been charged $5000 for your daily security bill.")
+                            async with db.execute("UPDATE security SET last_paid=? WHERE member=? and guild=?", (time.time(), member_id, guild_id)):
+                                pass
+                            await db.commit()
 
     @security_loop.before_loop
     async def before_security_loop(self):
