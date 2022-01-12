eaec1ef29d6c0f027df5d2ccb23f849ffcbf989d
Nebula1213 <fixingg@gmail.com>
2022-01-12 09:11:08 -0500

    change security to 50k per day

--- cogs/shop.py
+++ cogs/shop.py
@@ -28,9 +28,9 @@ class Shop(commands.Cog):
                 await inter.response.send_message("You do not have enough money.", ephemeral=True)
                 return
             await add_security(inter.guild_id, inter.author.id)
-            await set_balance(inter.guild_id, inter.author.id, bal - 5000)
+            await set_balance(inter.guild_id, inter.author.id, bal - 50000)
             await inter.response.send_message(
-                f"You have successfully bought the security plan. Balance: ${round(bal - 5000, 2)}", ephemeral=True)
+                f"You have successfully bought the security plan. Balance: ${round(bal - 50000, 2)}", ephemeral=True)
 
     @tasks.loop(seconds=10)
     async def security_loop(self):
@@ -51,16 +51,16 @@ class Shop(commands.Cog):
                         bal = await get_balance(guild_id, member_id)
                         guild = self.bot.get_guild(guild_id)
                         channel = guild.get_channel(await get_channel(guild_id, "bills"))
-                        if bal - 5000 <= 0:
+                        if bal - 50000 <= 0:
                             if channel is not None:
                                 await channel.send(
                                     f"{guild.get_member(member_id).mention}, you do not have enough money to pay for security, so your subscription has been canceled.")
                             await remove_security(guild_id, member_id)
                         else:
-                            await set_balance(guild_id, member_id, bal - 5000)
+                            await set_balance(guild_id, member_id, bal - 50000)
                             if channel is not None:
                                 await channel.send(
-                                    f"{guild.get_member(member_id).mention}, you have been charged $5000 for your daily security bill.")
+                                    f"{guild.get_member(member_id).mention}, you have been charged $50000 for your daily security bill.")
                             async with db.execute("UPDATE security SET last_paid=? WHERE member=? and guild=?", (time.time(), member_id, guild_id)):
                                 pass
                             await db.commit()
