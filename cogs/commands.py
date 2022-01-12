be38faa444658c2c4168c0a35335006c8673b534
Nebulizer1213 <fixingg@gmail.com>
2022-01-11 21:08:43 -0500

    Changes by IAmAnOof

--- cogs/commands.py
+++ cogs/commands.py
@@ -158,6 +158,72 @@ class Commands(commands.Cog):
             embed = disnake.Embed(title="Commands", color=disnake.Color.blurple(), description=msg)
         await inter.response.send_message(embed=embed, ephemeral=True)
 
+    @commands.guild_only()
+    @commands.cooldown(1, 120, commands.BucketType.member)
+    @commands.slash_command(name="beg")
+    async def beg_command(self, inter: disnake.ApplicationCommandInteraction):
+        bal = await get_balance(inter.guild_id, inter.author.id)
+        if bal >= 1000:
+            await inter.response.send_message("You are too rich to beg.", ephemeral=True)
+            return
+        if random.randrange(0, 25) == 1:
+            desc = "Congrats! Some generous rich man gave you $1000. You now have ${money}."
+            pay = 1000
+        else:
+            beg_list = [{"desc": "Some woman walking by pitied you, so she gave you ${pay}. You now have ${money}.",
+                         "pay": [10, 12, 13, 15, 17, 20]},
+                        {"desc": "An old man walked by and gave you ${pay}. You now have ${money}.",
+                         "pay": [5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 20, 22, 40]},
+                        {"desc": "It's your lucky day! You found ${pay} on the road!. You now have ${money}.",
+                         "pay": [1, 1, 1, 1, 1, 5, 5, 5, 10, 10, 10, 20, 50, 100]},
+                        {"desc": "You made nothing during your begging session today.", "pay": [0]},
+                        {
+                            "desc": "You tried to steal a purse from an old lady! You were fined ${pay}. You now have ${money}.",
+                            "pay": [-1, -3, -5, -7, -9, -10, -15]},
+                        {
+                            "desc": "You stole a purse from an old lady! You got ${pay}. You now have ${money}.",
+                            "pay": [10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 30, 30, 30, 30, 30, 50, 70, 90, 100, 150]}]
+            beg = random.choice(beg_list)
+            pay = random.choice(beg["pay"])
+            desc = beg["desc"]
+        msg = desc.format(pay=pay, money=round(bal + pay, 2))
+        await inter.response.send_message(msg)
+        await set_balance(inter.guild_id, inter.author.id, bal + pay)
+
+    @commands.guild_only()
+    @commands.cooldown(1, 60, commands.BucketType.member)
+    @commands.slash_command(name="invest")
+    async def invest_command(self, inter: disnake.ApplicationCommandInteraction, amount: int = commands.Param()):
+        bal = await get_balance(inter.guild_id, inter.author.id)
+        if bal < amount:
+            await inter.response.send_message("You do not have enough money.", ephemeral=True)
+            return
+        if random.randrange(0, 25) == 1:
+            desc = "The market spiked and you made $500. You now have ${money}."
+            percent = 500
+        else:
+            stock_list = [{
+                "desc": "There was a slight increase in the market, and you made ${pay} in return! You now have ${money}.",
+                "percent": [110, 111, 112, 113, 114, 115, 117, 120]},
+                {"desc": "The Stock Market crashed, and you lost ${pay}! You now have ${money}.",
+                 "percent": [10, 20, 30, 40, 50]},
+                {
+                    "desc": "There was a slight decrease in the market, and you lost ${pay}! You now have ${money}.",
+                    "percent": [90, 85, 75, 92, 93, 87, 65, 70, 76]},
+                {"desc": "There was a spike in the stock you invested in, and you made ${pay}! You now have ${money}.",
+                 "percent": [150, 165, 175, 180, 190, 200, 220, 160, 170, 155, 300]}]
+            stock = random.choice(stock_list)
+            percent = random.choice(stock["percent"])
+            desc = stock["desc"]
+        msg = desc.format(pay=round(abs((percent / 100) * amount - amount), 2),
+                          money=round(bal + (percent / 100) * amount - amount, 2))
+        await inter.response.send_message(msg)
+        await set_balance(inter.guild_id, inter.author.id, bal + (percent / 100) * amount - amount)
+
+    @commands.slash_command(name="invite")
+    async def invite_command(self, inter: disnake.ApplicationCommandInteraction):
+        await inter.response.send_message(
+            f"Invite Link: [click](https://discord.com/api/oauth2/authorize?client_id=929595364821074020&permissions=8&scope=bot%20applications.commands)")
 
 
 def setup(bot):
