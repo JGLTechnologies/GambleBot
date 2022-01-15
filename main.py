aeb752cb95fdca78b648153974b433e08ae163f5
Nebulizer1213 <fixingg@gmail.com>
2022-01-15 18:07:54 -0500

    update

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from disnake.ext import commands
 import time
 import logging
 
-logging.basicConfig(filename='jglbot.log', encoding='utf-8', level=logging.ERROR,
+logging.basicConfig(filename='GambleBot.log', encoding='utf-8', level=logging.ERROR,
                     format="[%(asctime)s] %(levelname)s: %(message)s", datefmt="%m-%d-%Y %I:%M:%S %p")
 TOKEN = os.environ.get("GambleBot_TOKEN")
 cogs = ["cogs.rps", "cogs.events", "cogs.commands", "cogs.credit", "cogs.shop", "cogs.blackjack"]
