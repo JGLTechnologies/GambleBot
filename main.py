3eb2713c0ff8aaac1faa5aba0a7aa70d6c59e360
Nebulizer1213 <fixingg@gmail.com>
2022-01-30 17:43:44 -0500

    update

--- main.py
+++ main.py
@@ -3,7 +3,10 @@ import os
 from disnake.ext import commands
 import time
 import logging
+from dotenv import load_dotenv
 
+
+load_dotenv()
 logging.basicConfig(filename='GambleBot.log', encoding='utf-8', level=logging.ERROR,
                     format="[%(asctime)s] %(levelname)s: %(message)s", datefmt="%m-%d-%Y %I:%M:%S %p")
 TOKEN = os.environ.get("GambleBot_TOKEN")
